2e5e0ee943d961c976bdb3f5b3bbc848c293811f
Michael Vieira <contact@mvieira.fr>
2020-04-23 17:03:48 +0200

    Add foreground handler for the NFC part

--- app/src/main/java/fr/mvieira/nrfc/MainActivity.kt
+++ app/src/main/java/fr/mvieira/nrfc/MainActivity.kt
@@ -1,15 +1,23 @@
 package fr.mvieira.nrfc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import fr.mvieira.nrfc.models.Scan
-import kotlin.collections.ArrayList
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private lateinit var listview: ListView
-
+    private lateinit var nfcAdapter: NfcAdapter
+    private lateinit var pendingIntent: PendingIntent
     private var scans: ArrayList<Scan> = ArrayList()
+    private var nfcAdapterAttempts: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +29,49 @@ class MainActivity : AppCompatActivity() {
         listview = findViewById(R.id.scan_history_listview)
         val listAdapter = ScanHistoryListAdapter(this, scans)
         listview.adapter = listAdapter
+
+        // Setup NFC reception handler
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        val intent = Intent(this, javaClass).apply {
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+
+        pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (nfcAdapter != null) {
+            if (!nfcAdapter.isEnabled && nfcAdapterAttempts < 2) {
+                nfcAdapterAttempts++
+
+                Toast.makeText(this, R.string.toast_nfc_not_enabled, Toast.LENGTH_LONG).show()
+
+                val intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                startActivity(intent)
+            } else if (!nfcAdapter.isEnabled && nfcAdapterAttempts > 1) {
+                Toast.makeText(this, R.string.toast_nfc_not_enabled_app_stopped, Toast.LENGTH_LONG)
+                    .show()
+
+                this.finish()
+                exitProcess(1)
+            }
+
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null)
+        }
+    }
+
+    public override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        val tagFromIntent: Tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+        val action = intent.action
+
+        Toast.makeText(this, "Intent handled!", Toast.LENGTH_SHORT).show()
+        //do something with tagFromIntent
+//        val intentt = Intent(this, this::class.java)
+//        startActivity(intentt)
     }
 }
